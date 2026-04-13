47f472642b2f19c8fbb2da1715645935cb9f5df9
yamamoto-misa-ki <misa0126y@gmail.com>
2026-04-13 23:07:48 +0900

    Add files via upload
    よろしくお願いいたします。

--- Main1.java
+++ Main1.java
@@ -1,14 +1,14 @@
 public class Main1 {
     public static void main(String[] args) {
-        // 1. 各商品の金額を変数に代入する
-        int bento = 480;
-        int ocha = 160;
-        int onigiri = 130;
+        // 1. 年齢を設定
+        int age = 18; 
 
-        // 2. 合計金額を計算する
-        int total = bento + ocha + onigiri;
-
-        // 3. 結果を表示する
-        System.out.println("合計金額は" + total + "円です。");
+        // 2. もし20歳以上なら
+        if (age >= 20) {
+            System.out.println("お酒を購入できます。");
+        } else {
+            // そうでなければ
+            System.out.println("お酒を購入できません。");
+        }
     }
 }
